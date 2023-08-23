97d0f145e943d28581e9282093b79acc125490f4
Vedant Sonawane <98942578+Vedant2s@users.noreply.github.com>
2023-08-23 10:19:14 +0530

    Update StudentDatabase.sh

--- StudentDatabase.sh
+++ StudentDatabase.sh
@@ -1,9 +1,5 @@
 clear
 while [ 1 ]
-do
-        echo "1.Create\n2.Display\n3.Insert\n4.Search\n5.Modify\n6.Delete\n7.exit"
-        echo "Ent
-while [ 1 ]
 do 
 	echo "1.Create\n2.Display\n3.Insert\n4.Search\n5.Modify\n6.Delete\n7.exit"
 	echo "Enter choice=\c"
@@ -28,7 +24,7 @@ do
 				echo "The Content Of File"
 				sort -n $fname
 			else
-				echo"File name does not exist"
+				echo "File name does not exist"
 			fi
 			;;
 		3)
@@ -50,8 +46,100 @@ do
 					echo "Record Inserted successfully"
 				fi
 			else
-				echo"The file does not exist"
+				echo "The file does not exist"
 			fi
+			;;
+		4)
+			echo -n "Enter File Name:"
+                        read fname
+                        if [ -e $fname ]
+                        then
+                                echo "Enter the roll number to be searched"
+                                read roll
+                                grep -w "$roll" $fname
+                                flag=$?
+                                if [ $flag -eq 0 ]
+                                then
+                                        echo "Record found"
+                                else
+                                        echo "Record NOT found"
+                                fi
+                        else
+                                echo "The file does not exist"
+                        fi
+			;;
+		5)
+                        echo -n "Enter File Name:"
+                        read fname
+                        if [ -e $fname ]
+                        then
+                                echo "Enter the roll number"
+                                read roll
+                                grep -w "$roll" $fname
+                                flag=$?
+                                if [ $flag -eq 0 ]
+                                then
+                                        echo "Enter the new Roll number and Name of student"
+                                        read newroll newname
+                                        grep -w "$newroll" $fname
+                                        flag=$?
+                                        if [ flag -eq 0 ]
+                                        then
+                                                echo "Record with the new entered roll number already exists"
+                                        else
+                                                grep -v "$roll" $fname >> temp
+                                                rm $fname
+                                                
+                                                cp temp $fname
+                                                
+                                                rm temp
+                                                
+                                                echo $newroll $newname >> $fname
+                                        fi
+                                else
+                                        echo "Record NOT found"
+                                fi
+                        else
+                                echo "The file does not exist"
+                        fi
+			;;
+		6)
+			5)
+                        echo -n "Enter File Name:"
+                        read fname
+                        if [ -e $fname ]
+                        then
+                                echo "Enter the roll number"
+                                read roll
+                                grep -w "$roll" $fname
+                                flag=$?
+                                if [ $flag -eq 0 ]
+                                then
+                                        echo "Enter the new Roll number and Name of student"
+                                        read newroll newname
+                                        grep -w "$newroll" $fname
+                                        flag=$?
+                                        if [ flag -eq 0 ]
+                                        then
+                                                echo "Record with the new entered roll number already exists"
+                                        else
+                                                grep -v "$roll" $fname >> temp
+                                                rm $fname
+                                                
+                                                cp temp $fname
+                                                
+                                                rm temp
+                                                
+                                                echo $newroll $newname >> $fname
+                                        fi
+                                else
+                                        echo "Record NOT found"
+                                fi
+                        else
+                                echo "The file does not exist"
+                        fi
+
+
 			;;
 		7)
 			exit
