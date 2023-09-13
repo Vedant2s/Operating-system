0bde7bbbb0e0717fac7f4d17f88b94eef4a570eb
Vedant Sonawane <98942578+Vedant2s@users.noreply.github.com>
2023-09-13 10:16:54 +0530

    Create FCFS.c

---
+++ FCFS.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+struct Procedure {
+	int Proc,at,bt,tt,wt;
+};
+struct Procedure p[10];
+void sort(int);
+void calculate(int);
+int main(){
+	int n;
+	printf("Enter number of processes you want to enter:\t");
+	scanf("%d",&n);
+	printf("Enter your Process Number, Arrival time , Burst Time\n");
+	for(int i=0;i<n;i++){
+		scanf("%d",&p[i].Proc);
+		scanf("%d",&p[i].at);
+		scanf("%d",&p[i].bt);
+	}
+	printf("PROCEDURE\tAT\tBT\n");
+	for(int i=0;i<n;i++)
+		printf("P%d\t\t%d\t%d\n",p[i].Proc,p[i].at,p[i].bt);
+	sort(n);
+	calculate(n);
+        printf("PROCEDURE\tAT\tBT\tTT\tWT\n");
+        for(int i=0;i<n;i++)
+                printf("P%d\t\t%d\t%d\t%d\t%d\n",p[i].Proc,p[i].at,p[i].bt,p[i].tt,p[i].wt);
+	return 0;
+
+}
+void sort(int n){
+	struct Procedure temp;
+	for(int i=0;i<n;i++){
+		for(int j=i+1;j<n;j++)
+		{
+			if(p[i].at>p[j].at){
+				temp=p[i];
+				p[i]=p[j];
+				p[j]=temp;
+			}
+		}	
+	}
+	printf("SORTED\n\nPROCEDURE\tAT\tBT\n");
+	for(int i=0;i<n;i++)
+		printf("P%d\t\t%d\t%d\n",p[i].Proc,p[i].at,p[i].bt);
+
+}
+void calculate(int n){
+	p[0].tt=p[0].at+p[0].bt;
+	for(int i=1;i<n;i++){
+		p[i].tt=p[i].bt+p[i-1].tt;
+	}
+	for(int i=0;i<n;i++){
+		p[i].tt=p[i].tt-p[i].at;
+		p[i].wt=p[i].tt-p[i].bt;
+	}
+
+}
