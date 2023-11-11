6ba01c7ac9df64fa2222758af35b3d8825c62ae2
Vedant Sonawane <98942578+Vedant2s@users.noreply.github.com>
2023-11-11 13:23:06 +0530

    Update reader_writer.c

--- reader_writer.c
+++ reader_writer.c
@@ -4,106 +4,134 @@
 #include <semaphore.h> 
 #include <stdint.h>
 #include <unistd.h>
- 
 
+// Define the number of readers and writers
 #define R 5
 #define W 5
+
+// Declare global variables
 int readcount;
 int writecount;
-//sem_t x;
-sem_t y;			
-pthread_mutex_t x;		
-sem_t wsem,rsem;	//mutual exclusion	
-int s=5;
-void *reader1(void *a);
-void *writer1(void *a);
-int main()
-{
-	int i,op;
-	
-	pthread_t thread_read[R],thread_write[W];
-	//sem_init(&x,0,1); 
-	  pthread_mutex_init(&x,NULL);	//initializeed to default value
-    	sem_init(&wsem,0,1);
-    	sem_init(&y,0,1); 
-    	sem_init(&rsem,0,1);
-    	
-    
-    	
-	
-	
-	
-    	do{
-	    	printf("Menu : 1. Readers have priority  2.exit ");
-	    	scanf("%d",&op);
-	    	switch(op)
-	    	{
-	    		case 1:readcount=0;
-	    			for(i=0;i<W;i++)
-				{
-					pthread_create(&thread_write[i],NULL, *writer1,(void *) (intptr_t) i);
-				}
-				for(i=0;i<R;i++)
-				{
-					pthread_create(&thread_read[i],NULL, *reader1,(void *) (intptr_t) i);
-				}
-				for(i=0;i<W;i++)
-				{
-					pthread_join(thread_write[i],NULL);
-				}
-				for(i=0;i<R;i++)
-				{
-					pthread_join(thread_read[i],NULL);
-				}
-	    			break;
-	    			
-	    		case 2: break;
-	    	}
-	    	
-    	}while(op!=2);
+
+// Declare semaphores
+sem_t wsem; // Protects the shared resource from concurrent writers
+sem_t rsem; // Protects the readcount variable from concurrent access
+pthread_mutex_t x; // Protects the shared resource from concurrent readers
+
+// Declare a shared variable
+int s = 5;
+
+// Define the reader function
+void *reader1(void *a) {
+  // Get the reader ID
+  int r = (intptr_t) a;
+
+  // Iterate for 5 times
+  for (int i = 0; i < 5; i++) {
+    // Wait for the read semaphore
+    sem_wait(&rsem);
+
+    // Acquire the mutex lock
+    pthread_mutex_lock(&x);
+
+    // Increment the readcount
+    readcount++;
+
+    // If the readcount is 1, then wait for the write semaphore
+    if (readcount == 1) {
+      sem_wait(&wsem);
+    }
+
+    // Release the mutex lock
+    pthread_mutex_unlock(&x);
+
+    // Release the read semaphore
+    sem_post(&rsem);
+
+    // Read the shared variable
+    printf("\t\tReader %d is reading: %d\n", r, s);
+
+    // Acquire the mutex lock
+    pthread_mutex_lock(&x);
+
+    // Decrement the readcount
+    readcount--;
+
+    // If the readcount is 0, then post the write semaphore
+    if (readcount == 0) {
+      sem_post(&wsem);
+    }
+
+    // Release the mutex lock
+    pthread_mutex_unlock(&x);
+
+    // Sleep for a random amount of time
+    sleep(rand() % 10);
+  }
 }
 
-void *reader1(void *a)
-{
-	//int r=(int)a;
-	int r = (intptr_t) a;
-	int i=0;
-	while (i<5){
-		//sleep(rand() % 10);
-		//sem_wait(&x);
-		pthread_mutex_lock(&x);
-		readcount++;
-		if(readcount == 1)
-		sem_wait(&wsem);
-		//sem_post(&x);
-		pthread_mutex_unlock(&x);
-		printf("\t\tReader %d is reading : %d \n",r,s);
-		//sem_wait(&x);
-		pthread_mutex_lock(&x);
-		readcount--;
-		if(readcount == 0)
-		sem_post(&wsem);
-		//sem_post(&x);
-		pthread_mutex_unlock(&x);
-		sleep(rand() % 10);
-		i++;
-	}
+// Define the writer function
+void *writer1(void *a) {
+  // Get the writer ID
+  int w = (intptr_t) a;
+
+  // Iterate for 2 times
+  for (int i = 0; i < 2; i++) {
+    // Wait for the write semaphore
+    sem_wait(&wsem);
+
+    // Increment the writecount
+    writecount++;
+
+    // Write to the shared variable
+    s += 5;
+    printf("Writer %d is writing: %d\n", w, s);
+
+    // Decrement the writecount
+    writecount--;
+
+    // Post the write semaphore
+    sem_post(&wsem);
+
+    // Sleep for a random amount of time
+    sleep(rand() % 10);
+  }
 }
 
-void *writer1(void *a)
-{
-	int w = (intptr_t) a;
-	//int w=(int)a;
-	int i=0;
-	while (i<2){
-	//sleep(rand() % 10);
-	sem_wait(&wsem);
-	s+=5;
-	printf("Writer %d is writing : %d \n",w,s);
-	sem_post(&wsem);
-	sleep(rand() % 10);
-	i++;
-	}
+// Define the main function
+int main() {
+  // Initialize the semaphores
+  sem_init(&wsem, 0, 1);
+  sem_init(&rsem, 0, 1);
+
+  // Initialize the mutex lock
+  pthread_mutex_init(&x, NULL);
+
+  // Create the reader and writer threads
+  pthread_t thread_read[R];
+  pthread_t thread_write[W];
+
+  for (int i = 0; i < R; i++) {
+    pthread_create(&thread_read[i], NULL, reader1, (void *) (intptr_t) i);
+  }
+
+  for (int i = 0; i < W; i++) {
+    pthread_create(&thread_write[i], NULL, writer1, (void *) (intptr_t) i);
+  }
+
+  // Join all the threads
+  for (int i = 0; i < R; i++) {
+    pthread_join(thread_read[i], NULL);
+  }
+
+  for (int i = 0; i < W; i++) {
+    pthread_join(thread_write[i], NULL);
+  }
+
+  // Destroy the semaphore and mutex lock
+  sem_destroy(&wsem);
+  sem_destroy(&rsem);
+  pthread_mutex_destroy(&x);
+
+  return 0;
 }
-
-
